257de8e3d8c74ba2acd7eb6bb2fe5523527216e0
Kamal Max <pmer93@gmail.com>
2018-03-02 00:28:00 +0330

    Constants moved to ChatBot class

--- randchatbot.py
+++ randchatbot.py
@@ -7,20 +7,15 @@ from twowaydict import TwoWayDict
 import threading
 import threading
 
-##########################################################################
-
-# Constants
-START_COMMAND_STR = 'start'
-END_COMMAND_STR = 'end'
-WAIT_MSG_STR = 'No users to match please wait'
-MATCHED_MSG_STR = 'You have matched chat'
-NO_PAIR_MSG = 'You have no pair'
-END_CHAT_MSG = 'Your chat is ended'
-
-##########################################################################
-
 
 class ChatBot:
+
+    START_COMMAND_STR = 'start'
+    END_COMMAND_STR = 'end'
+    WAIT_MSG_STR = 'No users to match please wait'
+    MATCHED_MSG_STR = 'You have matched chat'
+    NO_PAIR_MSG = 'You have no pair'
+    END_CHAT_MSG = 'Your chat is ended'
 
     def __init__(self, lock):
         self._unmatched_users = list()
@@ -35,14 +30,14 @@ class ChatBot:
             self._user_to_user[chat_id] = pair_id
             self._lock.release()
 
-            bot.send_message(chat_id, text=MATCHED_MSG_STR)
-            bot.send_message(pair_id, text=MATCHED_MSG_STR)
+            bot.send_message(chat_id, text=ChatBot.MATCHED_MSG_STR)
+            bot.send_message(pair_id, text=ChatBot.MATCHED_MSG_STR)
         else:
             self._lock.acquire()
             self._unmatched_users.append(chat_id)
             self._lock.release()
 
-            bot.send_message(chat_id, WAIT_MSG_STR)
+            bot.send_message(chat_id, ChatBot.WAIT_MSG_STR)
 
     def send_text_message_to_pair(self, bot, update):
         chat_id = update.message.chat_id
@@ -52,7 +47,7 @@ class ChatBot:
                 chat_id=pair_id,
                 text=update.message.text)
         else:
-            bot.send_message(chat_id, NO_PAIR_MSG)
+            bot.send_message(chat_id, ChatBot.NO_PAIR_MSG)
 
     def end_cmd(self, bot, update):
         chat_id = update.message.chat_id
@@ -63,10 +58,10 @@ class ChatBot:
             del self._user_to_user[chat_id]
             self._lock.release()
 
-            bot.send_message(chat_id, text=END_CHAT_MSG)
-            bot.send_message(pair_id, text=END_CHAT_MSG)
+            bot.send_message(chat_id, text=ChatBot.END_CHAT_MSG)
+            bot.send_message(pair_id, text=ChatBot.END_CHAT_MSG)
         else:
-            bot.send_message(chat_id, text=NO_PAIR_MSG)
+            bot.send_message(chat_id, text=ChatBot.NO_PAIR_MSG)
 
 
 def main():
@@ -75,8 +70,12 @@ def main():
 
     chat_bot = ChatBot(threading.Lock)
 
-    start_handler = CommandHandler(START_COMMAND_STR, chat_bot.start_cmd)
-    end_handler = CommandHandler(END_COMMAND_STR, chat_bot.end_cmd)
+    start_handler = CommandHandler(
+        ChatBot.START_COMMAND_STR, chat_bot.start_cmd)
+    
+    end_handler = CommandHandler(
+        ChatBot.END_COMMAND_STR, chat_bot.end_cmd)
+    
     msg_handler = MessageHandler(
         Filters.text, chat_bot.send_text_message_to_pair)
 
